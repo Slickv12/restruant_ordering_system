8141350e5b21d647d7e186633c8f9a88d1388049
Slickv12 <tanjiro12k@gmail.com>
2025-10-23 20:35:46 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+// Database Configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'restaurant_db');
+
+// Twilio API Configuration
+define('TWILIO_SID', 'AC93bb340470ff8ec7a01d6293272176bc');
+define('TWILIO_AUTH_TOKEN', '1a37bb785caca045aaa777c4c03abd56');
+define('TWILIO_PHONE_NUMBER', '+17044198559');
+
+// Error Reporting (Optional)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
